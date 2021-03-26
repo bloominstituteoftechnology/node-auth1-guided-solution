744938ef229c99d9fe57fafa088a72fe0facefea
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-26 16:54:36 +0100

    update

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -4,28 +4,26 @@ const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post('/register', (req, res, next) => {
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  const hash = bcrypt.hashSync(user.password, 8); // 2 ^ n
   user.password = hash;
 
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
     })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    .catch(next); // our custom err handling middleware in server.js will trap this
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', (req, res, next) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        req.session.user = user;
+        req.session.user = user; // this is the critical line. Session saved, cookie set on client
         res.status(200).json({
           message: `Welcome ${user.username}!, have a cookie!`,
         });
@@ -33,13 +31,11 @@ router.post('/login', (req, res) => {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
     })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    .catch(next);
 });
 
 router.get('/logout', (req, res) => {
-  if (req.session) {
+  if (req.session && req.session.user) {
     req.session.destroy(err => {
       if (err) {
         res.send(
@@ -50,7 +46,7 @@ router.get('/logout', (req, res) => {
       }
     });
   } else {
-    res.end();
+    res.send('excuse me, do I know you?');
   }
 });
 
