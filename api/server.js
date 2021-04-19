2e58e7e707214df8bcf61ea0310c8b42db37a4b3
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-04-19 16:21:36 +0200

    err middleware

--- api/server.js
+++ api/server.js
@@ -41,7 +41,7 @@ server.get('/', (req, res) => {
 });
 
 server.use((err, req, res, next) => { // eslint-disable-line
-  res.status(500).json({
+  res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
   });
