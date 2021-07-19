0e638f93966afa03e8f25d0ab6f6a196fae96df9
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-19 16:54:24 +0200

    bug

--- api/server.js
+++ api/server.js
@@ -47,7 +47,7 @@ server.use('*', (req, res, next) => {
   next({ status: 404, message: 'not found!' });
 });
 
-router.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
